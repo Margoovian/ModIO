e8a8e52851cfe79be8858aa1fbf5fa8ab93e23ab
Margoovian <devlyn.hendrie@gmail.com>
2025-04-26 17:47:49 +1000

    Removed old external demo project

--- External-Project/LineRenderer/Maths.cpp
+++
@@ -1,8 +0,0 @@
-#include "Maths.h"
-
-
-
-float Remap(float val, float oldMin, float oldMax, float newMin, float newMax)
-{
-	return ((val - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
-}
